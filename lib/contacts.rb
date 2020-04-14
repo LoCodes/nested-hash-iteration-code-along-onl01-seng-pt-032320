37eb3ae2acd9678857f6fedfb271c8a0170c498e
Loren Cipriano <loren25cipriano@gmail.com>
2020-04-14 04:03:35 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -30,8 +30,8 @@ end
   # contacts.each do |person, contact_details_hash|
   #   if person == "Freddy Mercury"
   #     contact_details_hash.each do |attribute, data|
-        binding.pry
-      end
-    end
-  end
-end
+#         binding.pry
+#       end
+#     end
+#   end
+# end
